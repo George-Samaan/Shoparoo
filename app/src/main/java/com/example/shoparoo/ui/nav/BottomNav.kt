5f59453d605527d2bb2b3a9b48a1a66c025e1872
George-Samaan <georgemichel07@icloud.com>
2024-10-04 20:14:22 +0300

    + update nav with filled icons

--- app/src/main/java/com/example/shoparoo/ui/nav/BottomNav.kt
+++ app/src/main/java/com/example/shoparoo/ui/nav/BottomNav.kt
@@ -1,32 +1,27 @@
 package com.example.shoparoo.ui.nav
 
-//noinspection UsingMaterialAndMaterial3Libraries
-//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.foundation.layout.height
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.BottomNavigation
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Category
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.shoparoo.R
 import com.example.shoparoo.ui.theme.Purple40
 
-
-sealed class BottomNav(val route: String, val icon: ImageVector, val label: String) {
-    object Home : BottomNav("home", Icons.Filled.Home, "Home")
-    object Categories : BottomNav("categories", Icons.Filled.Category, "Categories")
-    object Cart : BottomNav("cart", Icons.Filled.ShoppingCart, "Cart")
-    object Profile : BottomNav("profile", Icons.Filled.Person, "Profile")
+sealed class BottomNav(val route: String, val icon: Int, val label: String) {
+    object Home : BottomNav("home", R.drawable.ic_home, "Home")
+    object Categories : BottomNav("categories", R.drawable.ic_category, "Categories")
+    object Cart : BottomNav("cart", R.drawable.ic_cart, "Cart")
+    object Profile : BottomNav("profile", R.drawable.ic_profile, "Profile")
 }
 
 @Composable
@@ -36,14 +31,15 @@ fun BottomNavigationBar(navController: NavController) {
     )
 
     BottomNavigation(
-        modifier = Modifier.height(60.dp), backgroundColor = Color(0xFFEFEEEE)
+        modifier = Modifier.height(70.dp), backgroundColor = Color(0xFFEFEEEE)
     ) {
         val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
         items.forEach { item ->
             val isSelected = currentRoute == item.route
             BottomNavigationItem(icon = {
                 Icon(
-                    imageVector = item.icon, contentDescription = null,
+                    painter = painterResource(id = item.icon),
+                    contentDescription = item.label,
                     tint = if (isSelected) Purple40 else Color.Gray
                 )
             }, label = {
