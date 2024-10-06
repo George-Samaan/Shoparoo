2e52f1e403f071e0ac11ef2f0181e0de9a0675b9
George-Samaan <georgemichel07@icloud.com>
2024-10-06 03:52:52 +0300

    + sync files

--- app/src/main/java/com/example/shoparoo/ui/shoppingCart/ShoppingCart.kt
+++ app/src/main/java/com/example/shoparoo/ui/shoppingCart/ShoppingCart.kt
@@ -23,10 +23,12 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -234,7 +236,7 @@ fun ApplyCoupons(productList: List<Product>, onApplyCoupon: (Double) -> Unit) {
             modifier = Modifier
                 .padding(start = 8.dp)
                 .height(56.dp), // Adjust height to match EditText
-            colors = ButtonDefaults.buttonColors(Color(0xFF673AB7))
+            colors = ButtonDefaults.buttonColors(Color.Black)
         ) {
             Text(text = "Apply", color = Color.White)
         }
