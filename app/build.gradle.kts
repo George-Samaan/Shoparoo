8f4b0973b2ddc150bc0ad6b45a05235df10cb36e
En.Galal Ahmed <ga71387@gmail.com>
2024-10-08 12:23:59 +0300

    Update payment. Get current location. Set location from map.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,8 +105,14 @@ dependencies {
 
     // OkHttp Logging (optional, for debugging)
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
 
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 
+    implementation("com.github.mumayank:AirLocation:2.5.2")
+
+    implementation("com.github.KeepSafe:TapTargetView:1.13.2")
 
 
 
