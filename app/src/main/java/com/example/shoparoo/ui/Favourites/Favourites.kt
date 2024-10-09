c349a119baf568b1ee04112fc6b8bb63f784ec6d
gemb0_0 <agamal00500@gmail.com>
2024-10-09 23:39:08 +0300

    favourites screen

---
+++ app/src/main/java/com/example/shoparoo/ui/Favourites/Favourites.kt
@@ -0,0 +1,184 @@
+package com.example.shoparoo.ui.Favourites
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.shoparoo.R
+import com.example.shoparoo.data.db.remote.RemoteDataSourceImpl
+import com.example.shoparoo.data.network.ApiClient
+import com.example.shoparoo.data.repository.RepositoryImpl
+import com.example.shoparoo.model.ProductsItem
+import com.example.shoparoo.ui.auth.view.ReusableLottie
+import com.example.shoparoo.ui.homeScreen.view.ProductCard
+import com.example.shoparoo.ui.productScreen.view.ProductGrid
+import com.example.shoparoo.ui.productScreen.view.TopBar
+
+@Composable
+fun Favourites(navController: NavController) {
+
+    val viewModel: FavouritesViewModel = viewModel(
+        factory = FavouritesViewModelFactory(
+            repository = RepositoryImpl(
+                remoteDataSource = RemoteDataSourceImpl(apiService = ApiClient.retrofit)
+            )
+        )
+    )
+
+    val favProducts = viewModel.productItems.collectAsStateWithLifecycle()
+
+    // may run multiple times
+    LaunchedEffect(Unit) {
+        viewModel.getFavourites()
+    }
+
+    if (favProducts.value.isEmpty()) {
+        Column(
+            Modifier
+                .padding(top = 100.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+       //   Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "Favourites", modifier = Modifier.size(300.dp))
+
+            ReusableLottie(R.raw.empty, null,size =  400.dp)
+            Text(text = "No favourites yet", fontSize = 35.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+
+
+
+        TopBar(navController = navController, title = "Favourites")
+    Column(
+        Modifier
+            .padding(top = 100.dp)
+            .fillMaxSize()
+          ,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        //galal handle currency conversion
+    //  ProductGridd(favProducts.value, navController, "gg", 1.0f, emptyMap(),true,viewModel)
+
+        ProductGrid(favProducts.value, navController, "gg", 1.0f, emptyMap(),true,
+            viewModel = viewModel
+            )
+
+    }
+}
+
+
+@Composable
+fun ProductGridd(
+    filteredProducts: List<ProductsItem>,
+    navController: NavController?,
+    selectedCurrency: String,
+    conversionRate: Float,
+    currencySymbols: Map<String, String>,
+    inFav: Boolean = false,
+    viewModel: FavouritesViewModel,
+
+    ) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(filteredProducts.size) { index ->
+            val product = filteredProducts[index]
+            val fullTitle = product.title ?: "Unknown"
+            val productName = fullTitle.split("|").getOrNull(1)?.trim() ?: fullTitle
+            val priceInUSD = product.variants?.get(0)?.price?.toDoubleOrNull() ?: 0.0
+            val convertedPrice = priceInUSD * conversionRate
+            val formattedPrice = String.format("%.2f", convertedPrice)
+
+            ProductCard(
+                productName = productName,
+                productPrice = "$formattedPrice",
+                productImage = product.images?.firstOrNull()?.src ?: "",
+                onClick = {
+                    navController!!.navigate("productDetails/${product.id}")
+                },
+                currencySymbol = currencySymbols.getOrDefault(selectedCurrency, "$"),
+                inFav = inFav,
+                onClickDeleteFav = {
+                    Log.i("FavouritesViewModeldeleteeeee", "onClickDeleteFav: ${product.id}")
+                  //  viewModel.deleteFav(product.id)
+                    viewModel.getFavourites(true, product.id!!)
+
+
+                })
+
+
+        }
+    }
+}
+
+
+
+
+
+/*
+@Composable
+fun myProductGrid(
+    filteredProducts: List<Triple<String, String, String>>,
+    navController: NavController?,
+    selectedCurrency: String,
+    conversionRate: Float,
+    currencySymbols: Map<String, String>
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(filteredProducts.size) { index ->
+            val product = filteredProducts[index]
+            val fullTitle = filteredProducts.first() ?: "Unknown"
+            val productName = "dfdf"
+            val priceInUSD = 10.0f
+            val convertedPrice = "riceInUSD * conversionRate"
+            val formattedPrice = "%.2f"
+
+            ProductCard(
+                productName = product.second,
+                productPrice = product.third.toString(),
+                productImage = product.first,
+                onClick = {
+                   // navController!!.navigate("productDetails/${product.id}")
+                },
+                currencySymbol = currencySymbols.getOrDefault(selectedCurrency, "$")
+
+            )
+        }
+    }
+}
+*/
---
+++ app/src/main/java/com/example/shoparoo/ui/Favourites/FavouritesViewModel.kt
@@ -0,0 +1,156 @@
+package com.example.shoparoo.ui.Favourites
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.shoparoo.data.network.ApiState
+import com.example.shoparoo.data.repository.Repository
+import com.example.shoparoo.model.DraftOrderDetails
+import com.example.shoparoo.model.DraftOrderRequest
+import com.example.shoparoo.model.DraftOrderResponse
+import com.example.shoparoo.model.ImagesItem
+import com.example.shoparoo.model.ProductsItem
+import com.example.shoparoo.model.VariantsItem
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+class FavouritesViewModel(private val repository: Repository) : ViewModel() {
+    private val _draftOrderFav = MutableStateFlow<ApiState>(ApiState.Loading)
+    //  val draftOrderFav = _draftOrderFav.asStateFlow()
+
+    private val _productItems: MutableStateFlow<MutableList<ProductsItem>> =
+        MutableStateFlow(mutableListOf())
+    val productItems = _productItems.asStateFlow()
+
+    // var favProducts: StateFlow< MutableList<Triple<String,String,String>>> = MutableStateFlow(mutableListOf())
+    var xXx: MutableStateFlow<MutableList<ProductsItem>> = MutableStateFlow(mutableListOf())
+
+    val userMail by lazy {
+        FirebaseAuth.getInstance().currentUser?.email
+    }
+
+    fun getFavourites(clkDelete: Boolean = false, id: Long = 0) {
+        viewModelScope.launch {
+            _draftOrderFav.value = ApiState.Loading
+            repository.getDraftOrder().catch {
+                _draftOrderFav.value = ApiState.Failure(it.message ?: "Unknown Error")
+            }.collect {
+                _draftOrderFav.value = ApiState.Success(it)
+                var filter = filterByUser(it, userMail)
+
+                if (filter.first) { //handle this in ui
+                    Log.d("FavouritesViewModel", "Favourites found")
+                    if (clkDelete) {
+                        deleteItem(filter.second!!, id)
+                    } else
+                        getFavData(filter.second)
+                } else {
+                    _productItems.value = mutableListOf()
+                    Log.d("FavouritesViewModel", "Favourites not found")
+                }
+
+
+            }
+        }
+
+    }
+
+    private fun getFavData(second: DraftOrderDetails?) {
+        for (lineItem in second!!.line_items) {
+            Log.i("FavouritesViewModelwwwwwwww", "getFavData: ${lineItem.product_id}")
+            _productItems.value.add(
+                ProductsItem(
+                    title = lineItem.title,
+                    id = lineItem.product_id,
+                    variants = listOf(
+                        VariantsItem(
+                            title = lineItem.title,
+                            id = lineItem.variant_id.toLong(),
+                            price = lineItem.price
+                        )
+                    ),
+                    images = listOf(
+                        ImagesItem(
+                            src = lineItem.properties[0].value
+                        )
+                    )
+                )
+            )
+        }
+    }
+
+    fun deleteFav(id: Long?) {
+        Log.i("FavouritesViewModeldelete", "deleteFav: $id")
+
+        //   getFavourites(false, id!!)
+//        viewModelScope.launch {
+//            _draftOrderFav.value = ApiState.Loading
+//            repository.getDraftOrder().catch {
+//                _draftOrderFav.value = ApiState.Failure(it.message ?: "Unknown Error")
+//            }.collect {
+//                _draftOrderFav.value = ApiState.Success(it)
+//                var filter = filterByUser(it, userMail)
+//
+//                if (filter.first) { //handle this in ui
+//                    deleteItem(filter.second, id)
+//                } else {
+//                    Log.d("FavouritesViewModeldelete", "Favourites not found")
+//                }
+//            }
+//        }
+
+
+    }
+
+    private fun deleteItem(second: DraftOrderDetails, id: Long?) {
+        viewModelScope.launch {
+            for (lineItem in second.line_items) {
+                if (lineItem.product_id == id) {
+                    second.line_items.remove(lineItem)
+                    break
+                }
+            }
+
+            if (second.line_items.isEmpty()) {
+                repository.deleteDraftOrder(second.id!!)
+            } else {
+                val order = DraftOrderRequest(second)
+                repository.updateDraftOrder(order)
+            }
+
+            // Update the productItems state after deletion to trigger recomposition
+            _productItems.value = _productItems.value.filter { it.id != id }.toMutableList()
+        }
+    }
+
+
+}
+
+fun filterByUser(
+    draftOrderResponse: DraftOrderResponse,
+    userMail: String?
+): Pair<Boolean, DraftOrderDetails?> {
+    var favExists: Boolean = false
+    var UserdraftOrder: DraftOrderDetails? = null
+    for (draftOrder in draftOrderResponse.draft_orders!!) {
+        if (draftOrder.email == "FAV_" + userMail) {
+            UserdraftOrder = draftOrder
+            favExists = true
+        }
+    }
+    return Pair(favExists, UserdraftOrder)
+}
+
+
+class FavouritesViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(FavouritesViewModel::class.java)) {
+            return FavouritesViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
