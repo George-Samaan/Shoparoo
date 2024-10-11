8a31c85c86983ae9273ea3dfdb9e246dd4f26e30
George-Samaan <georgemichel07@icloud.com>
2024-10-11 14:58:36 +0300

    + fix bugs

--- app/src/main/java/com/example/shoparoo/ui/shoppingCart/view/ShoppingCart.kt
+++ app/src/main/java/com/example/shoparoo/ui/shoppingCart/view/ShoppingCart.kt
@@ -14,12 +14,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,22 +40,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.example.shoparoo.R
-import com.example.shoparoo.ui.theme.primary
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.window.DialogProperties
+import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import com.example.shoparoo.R
 import com.example.shoparoo.model.LineItem
 import com.example.shoparoo.ui.checkOut.AppHeader
+import com.example.shoparoo.ui.productScreen.view.LoadingIndicator
 import com.example.shoparoo.ui.shoppingCart.viewModel.ShoppingCartViewModel
+import com.example.shoparoo.ui.theme.primary
 import kotlinx.coroutines.delay
 
 
@@ -65,7 +65,7 @@ fun ShoppingCartScreen(
 
     LaunchedEffect(Unit) {
         viewModel.getCartItems()
-        delay(1500)
+        delay(900)
         isLoading.value = false
 
         if (cartItems.isEmpty()) {
@@ -98,7 +98,7 @@ fun ShoppingCartScreen(
                     .padding(16.dp),
                 contentAlignment = Alignment.Center
             ) {
-                CircularProgressIndicator()
+                LoadingIndicator()
             }
         }
     } else {
